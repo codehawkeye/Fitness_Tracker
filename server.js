fc0f5ed2b55ba3743b1dfb5372bab0cbecbe5e53
Mike Carter <elite.mikecarter@gmail.com>
2021-02-13 14:52:24 -0500

    added express route

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-
-
+const logger = require("morgan")
 
 const PORT = 3002;
-
-
 const app = express();
+
+app.use(logger("dev"));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
     console.log(`Your connected on Port http://localhost:${PORT}`);
