67c8c047b0e17aa2dc42a672fc1a8f6b206faf2e
Mike Carter <elite.mikecarter@gmail.com>
2021-02-16 19:15:57 -0500

    added connection to heroku

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 // middleware functions
-Mongoose.connect("mongodb://localhost/workout", {
+Mongoose.connect(
+process.env.MONGODB_URI || 'mongodb://localhost/sheltered-journey-69225', {
     useNewUrlParser: true,
     useFindAndModify: false,
+        useCreateIndex: true,
+        useFindAndModify: false
 });
 
 app.use(require("./routes/htmlRoutes"))
